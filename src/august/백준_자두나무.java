5b3a2e5c4221a0f6f949f8d2c36e89b9b7bb0430
soony <dhsmf4245@gmail.com>
2025-08-02 11:08:21 +0900

    TIL

--- src/august/백준_자두나무.java
+++ src/august/백준_자두나무.java
@@ -12,68 +12,35 @@ public class 백준_자두나무 {
         t = sc.nextInt();
         w = sc.nextInt();
         dp = new int[t+1][w+1];
-        arr = new int[t];
-        for (int i = 0; i < t; i++) {
+        arr = new int[t+1];
+        for (int i = 1; i <= t; i++) {
             arr[i] = sc.nextInt();
         }
-        for(int i = 0; i < t; i++){
+        for(int i = 1; i < t+1; i++){
+            for(int j = 0; j <= w; j++){
+                int now;
+                if (j % 2 == 0) now = 1;
+                else now = 2;
 
-        }
-        if (arr[0] == 0) dp[0][w] = 1;
-        else dp[0][w] = 1;
-
-        func(0,w,0);
-        func(0,w,1);
-        System.out.println(Arrays.deepToString(dp));
-
-    }
-    static int switchNum(int i){
-        if (i == 1) return 2;
-        else return 1;
-    }
-    // rest는 무빙 남은 횟수 now는 현재 위치
-    static void func(int idx, int rest, int now){
-        if (idx >= t-1) return;
-        System.out.println("idx: "+ idx);
-        // rest가 1보다 큰 경우
-
-        // rest가 1보다 작은 경우
-        //현재 위치 일치
-        if (arr[idx+1] == now){
-            // 안 움직인다.
-            dp[idx+1][rest] = Math.max(dp[idx+1][rest], dp[idx][rest]+1);
-            func(idx+1, rest, now);
-            // 움직인다.
-            if (rest > 1){
-                dp[idx+1][rest-1] = Math.max(dp[idx][rest]+1, dp[idx+1][rest-1]);
-                func(idx+1, rest-1, switchNum(now));
-            }
-        } else if (arr[idx+1] != now) {
-            dp[idx+1][rest] = Math.max(dp[idx+1][rest], dp[idx][rest]);
-            func(idx+1, rest, now);
-            if (rest > 1){
-                dp[idx+1][rest-1] = Math.max(dp[idx][rest], dp[idx+1][rest-1]);
-                func(idx+1, rest-1, switchNum(now));
+                if (j == 0){
+                    dp[i][j] = dp[i-1][j];
+                    if (arr[i] == 1) dp[i][j]++;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]);
+                    if (arr[i] == now) dp[i][j] ++; 
+                }
             }
         }
-
-//        if (rest > 1){
-//            if (arr[idx+1] == now){
-//                dp[idx+1][rest] = Math.max(dp[idx][rest]+1, dp[idx+1][rest]);
-//            }
-//            else {
-//                dp[idx+1][rest-1] = Math.max(dp[idx][rest]+1, dp[idx+1][rest-1]);
-//            }
-//        }
-//        else {
-//            if (arr[idx+1] == now){
-//                dp[idx+1][rest] = Math.max(dp[idx+1][rest], dp[idx][rest]+1);
-//            }
-//            else {
-//                dp[idx+1][rest] = Math.max(dp[idx+1][rest], dp[idx][rest]+1);
-//
-//            }
-//        }
+        int answer = 0;
+        for(int i = 0; i <= w; i++){
+            answer = Math.max(answer, dp[t][i]);
+        }
+        System.out.println(answer);
+//        System.out.println(Arrays.deepToString(dp));
 
     }
+
+
+
 }
---
+++ src/august/백준_자두나무_v2.java
@@ -0,0 +1,9 @@
+package august;
+
+import java.util.*;
+
+public class 백준_자두나무_v2 {
+    public static void main(String[] args) {
+
+    }
+}
